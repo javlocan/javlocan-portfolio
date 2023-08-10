b2a49f36b1176c3609715709d32c9cfd98ee2135
javlocan <javlocan.dev@gmail.com>
2023-08-10 13:20:16 +0200

    First commit

---
+++ src/app/scenes/About.tsx
@@ -0,0 +1,27 @@
+export const About = () => {
+  return (
+    <section className="container" id="about">
+      <div className="about--grid">
+        <h2>Hi! This is me, javi</h2>
+        <img src="/perfil.jpeg" alt="My profile photo" className="photo" />
+        <p>
+          A los 13 años destripaba plantillas de Tumblr. CSS2, HTML4...
+          ¿Remember Microsoft Frontpage? Lo dejé cuando 10.000 personas habían
+          elegido la más básica, fea y poco trabajada de todas las que hice.
+          Diseñaba firmas para foros en la vanguardia del Photoshop (CS6) y
+          jugaba con wordpress y blogspot cuando se hacían la competencia.
+          Javascript era, a menudo, esa cosa que hacía romperse las páginas y
+          las llenaba de historias molestas que cargaban extremadamente lento.
+          En ingeniería aprendí algoritmia con C. Descubrí cuánto disfrutaba
+          resolviendo problemas lógicos y la creatividad que podía poner en
+          ello. <br />
+          <br />
+          Ha llovido desde entonces. Ahora me dedico a construir soluciones
+          lógicas y visuales para la web, poniendo la intuitividad y
+          accesibilidad en el centro. Una tecnología con interactividad sencilla
+          y natural, algo que todas podamos usar.
+        </p>
+      </div>
+    </section>
+  );
+};
---
+++ src/app/scenes/Landing.tsx
@@ -0,0 +1,17 @@
+export const Landing = () => {
+  return (
+    <header className="title">
+      <div className="title--grid">
+        <h1>JAVIER LOPEZ CANTERO</h1>
+        <h4>.developer</h4>
+        <div className="title--quote">
+          <h6>
+            Tras un tiempo explorando profesionalmente el mundo social, he
+            decidido volver a mis raíces de ingeniero como desarrollador
+            <b> frontend </b>y<b> fullstack.</b>
+          </h6>
+        </div>
+      </div>
+    </header>
+  );
+};
